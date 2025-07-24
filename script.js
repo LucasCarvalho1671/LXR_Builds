52bc91b795685f0d386896d9435f32716eaa2f82
LucasCarvalhoDEV <killonferreira@gmail.com>
2025-07-24 08:02:00 -0300

    Add Delta

--- script.js
+++ script.js
@@ -87,9 +87,9 @@ const perguntarAI = async (question, game, apiKey) => {
 
   const perguntaBDO = `
     ## Especialidade
-    Voce e um especialista assistente de meta para o jogo Black Desert Online (BDO)
+    Voce e um especialista assistente de meta para o jogo ${game}
     ## Tarefa
-    Voce deve responder as perguntas do usuario com base no seu conhecimento do jogo (classes, builds de equipamentos, skills, grind spots, chefes, sistemas de progressão, dicas de economia), e dicas.
+    Voce deve responder as perguntas do usuario com base no seu conhecimento do jogo (classes, builds de equipamentos, skills, grind spots, chefes, sistemas de progressão, dicas de economia), e dicas, levando em consideração as mecanicas e novidades da temporada no momento.
     ## Regras
     - Se voce nao sabe a resposta, responda com 'Não sei' e nao tente inventar uma resposta.
     - Se a pergunta nao esta relacionada ao jogo, responda com 'Essa pergunta não esta relacionada ao jogo'
@@ -103,6 +103,29 @@ const perguntarAI = async (question, game, apiKey) => {
     ---
     Aqui esta a pergunta do usuario ${question}
   `;
+  const perguntaDelta = `
+    ## Especialidade
+    Voce e um especialista assistente de meta para o jogo Delta Force (qualquer versão específica, se souber, adicione aqui)
+
+    ## Tarefa
+    - Voce deve responder as perguntas do usuario com base no seu conhecimento do jogo (armas, equipamentos, mapas, táticas de combate, modos de jogo, estratégias de infiltração e eliminação), e dicas.
+    - Se o usuario perguntar sobre builds de armas, deve informar os componentes e o codigo referente para o usuario importar no jogo.
+
+    ## Regras
+    - Se voce nao sabe a resposta, responda com 'Não sei' e nao tente inventar uma resposta.
+    - Se a pergunta nao esta relacionada ao jogo, responda com 'Essa pergunta não esta relacionada ao jogo'
+    - Considere a data atual ${new Date().toLocaleDateString()}
+    - Faça pesquisas atualizadas sobre o patch atual (se aplicável), baseado na data atual, para dar uma resposta coerente
+    - Nunca responda itens que voce nao tenha certeza de que existe no patch atual.
+
+    ## Resposta
+    - Economiza na resposta, seja direto e responda no maximo 700 caracteres
+    - Responda em markdown
+    - Não presisa fazer saudação ou despedida, apeas responda o que o usuario esta querendo.
+    ---
+    Aqui esta a pergunta do usuario ${question}
+  `;
+
   // Declaração da variável 'pergunta'
   let pergunta = "";
 
@@ -116,6 +139,8 @@ const perguntarAI = async (question, game, apiKey) => {
     pergunta = perguntatft;
   } else if (game === "bdo") {
     pergunta = perguntaBDO;
+  } else if (game === "delta") {
+    pergunta = perguntaDelta;
   } else {
     // Caso nenhum jogo específico seja selecionado ou a opção seja inválida
     pergunta = `Você é um assistente de IA. Responda à seguinte pergunta: ${question}`;
@@ -190,14 +215,18 @@ const enviarFormulario = async (event) => {
 form.addEventListener("submit", enviarFormulario);
 
 // Registro do Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js') // Caminho para o seu service worker
-      .then(registration => {
-        console.log('Service Worker registrado com sucesso:', registration.scope);
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js") // Caminho para o seu service worker
+      .then((registration) => {
+        console.log(
+          "Service Worker registrado com sucesso:",
+          registration.scope
+        );
       })
-      .catch(error => {
-        console.error('Falha ao registrar Service Worker:', error);
+      .catch((error) => {
+        console.error("Falha ao registrar Service Worker:", error);
       });
   });
 }
