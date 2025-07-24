14892df4f548bb7ff06036fa9ee9a0950e521c32
LucasCarvalhoDEV <killonferreira@gmail.com>
2025-07-24 10:22:50 -0300

    Versão 1.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "assistente-meta-cache-v1.1"; // Nome do seu cache, mude a versão se alterar os arquivos
+const CACHE_NAME = "assistente-meta-cache-v1.2"; // Nome do seu cache, mude a versão se alterar os arquivos
 const urlsToCache = [
   "/",
   "/index.html",
