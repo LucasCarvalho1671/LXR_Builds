23a4afa87ad9bd23112d4ca9d08730fa0e0a9afe
LucasCarvalhoDEV <killonferreira@gmail.com>
2025-07-31 20:30:00 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "assistente-meta-cache-v2.8"; // Mude a versão se alterar os arquivos
+const CACHE_NAME = "assistente-meta-cache-v2.9"; // Mude a versão se alterar os arquivos
 const urlsToCache = [
   "/",
   "/index.html",
