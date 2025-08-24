4b97b93e59a85f0e2c4a1e5e11ccbfacd2f03661
LucasCarvalhoDEV <killonferreira@gmail.com>
2025-08-24 15:36:33 -0300

    ajust logo

--- backend/public/sw.js
+++ backend/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "assistente-meta-cache-v2.45"; // Mude a versão se alterar os arquivos
+const CACHE_NAME = "assistente-meta-cache-v2.46"; // Mude a versão se alterar os arquivos
 const urlsToCache = [
   "/",
   "/index.html",
